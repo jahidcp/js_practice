5602dc464c66bbf2af6b817bbc6b5d8b4dbd152a
jahidcp <programmingloverjahid@gmail.com>
2023-01-26 00:07:57 +0600

    My First Javascript practice Day1

---
+++ practice_day_01.js
@@ -0,0 +1,54 @@
+var money = 40;
+var asset_pending = 1000;
+var losing_money = 500;
+console.log(money)
+
+var newToken = 833;
+
+console.log(newToken)
+
+// Changing Value
+
+newToken = 950;
+
+console.log(newToken)
+
+// Update amount of Token\
+newToken = newToken + 150;
+console.log(newToken)
+
+var firstName = 'MOHAMMAD JAHIDUl';
+var lastName = 'ISLAM';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName)
+
+// Concatenation
+
+var price = 390;
+var anotherPrice = '456.25';
+var totalPrice = price + anotherPrice;
+console.log(totalPrice);
+
+var changeAnotherPrice = parseInt(anotherPrice);
+console.log(changeAnotherPrice)
+
+var changeAnotherPrice2 = parseFloat(anotherPrice);
+console.log(changeAnotherPrice2)
+
+// tofixed or find way data type
+
+var num1 = 'jahid';
+console.log(num1)
+
+var num2 = 0.1;
+var num3 = 0.2;
+var sum = num2 + num3;
+console.log(sum)
+
+var sum = sum.toFixed(1);
+console.log(sum)
+
+// Change To Float Number
+
+var sum = parseFloat(sum);
+console.log(sum)
